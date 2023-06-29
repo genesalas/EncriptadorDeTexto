b7db199d39446c8bff10ba4dea39314b8e5672f5
Genesis <genesissalass22@gmail.com>
2023-06-29 01:58:35 -0300

    Esta es mi versión del challenge encriptador Alura

---
+++ script.js
@@ -0,0 +1,74 @@
+let textArea = document.querySelector(".textarea-encriptar");
+let mensaje = document.querySelector(".textarea-desencriptar");
+let btnEncriptar = document.querySelector(".btn-encriptar");
+let textoSectionDos = document.querySelector(".texto-section-dos");
+let btnDesencriptar = document.querySelector(".btn-desencriptar");
+let btnCopiar = document.querySelector(".btn-copiar");
+let message = document.querySelector("#message");
+
+//La letra "e" es convertida para "enter"
+//La letra "i" es convertida para "imes"
+//La letra "a" es convertida para "ai"
+//La letra "o" es convertida para "ober"
+//La letra "u" es convertida para "ufat"
+
+function botonEncriptar(){
+    let textoInicial = encriptar(textArea.value);
+    mensaje.value = textoInicial;
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none";
+}
+
+ btnEncriptar.onclick= botonEncriptar;
+ 
+ function ocultar(){
+    textoSectionDos.classList.add("ocultar");
+}
+
+function encriptar(fraseEncriptada){
+    ocultar()
+    let llaves = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+    fraseEncriptada = fraseEncriptada.toLowerCase();
+
+    for(let i = 0; i < llaves.length; i++){
+        if(fraseEncriptada.includes(llaves[i][0])){
+            fraseEncriptada = fraseEncriptada.replaceAll(llaves[i][0], llaves[i][1]);
+        }
+    }
+    return fraseEncriptada;
+}
+
+function botonDesencriptar(){
+    let textoInicial = desencriptar(textArea.value);
+    mensaje.value = textoInicial;
+    textArea.value = "";
+}
+
+btnDesencriptar.onclick = botonDesencriptar; 
+
+function desencriptar(fraseDesencriptada){
+    let llaves = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+    fraseDesencriptada = fraseDesencriptada.toLowerCase();
+
+    for(let i = 0; i < llaves.length; i++){
+        if(fraseDesencriptada.includes(llaves[i][1])){
+            fraseDesencriptada = fraseDesencriptada.replaceAll(llaves[i][1], llaves[i][0]);
+        }
+    }
+    return fraseDesencriptada;
+}
+
+btnCopiar.onclick = copiar;
+
+function copiar () {
+    let texto = mensaje.value;
+    navigator.clipboard.writeText(texto);
+  }
+
+  
+
+
+  
+  
+
+
